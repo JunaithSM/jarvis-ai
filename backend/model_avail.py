e0bf91b95510f5a085b7d852c98e9293b11dc630
Vishal V <vishalv6010@gmail.com>
2026-02-01 21:48:08 +0530

    removed hard coded api key

--- backend/model_avail.py
+++ backend/model_avail.py
@@ -3,7 +3,7 @@ from src.config.env import GROQ_API_KEY
 
 print("Connecting to Groq...")
 
-client = Groq(api_key=GROQ_API_KEY)
+client = Groq(api_key="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")
 
 models = client.models.list()
 
